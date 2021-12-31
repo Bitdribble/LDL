3014eecfe4b5561c3cbf0f63e20d9a9a557c09d1
Andrei Radulescu-Banu <44415560+andrei-radulescu-banu@users.noreply.github.com>
2021-12-31 17:38:36 -0500

    Devel

--- docker/docker_start.sh
+++ docker/docker_start.sh
@@ -4,8 +4,11 @@ DOCKER_NAME=ldl
 
 # Is an instance already running?
 if [[ $(docker ps --filter name=$DOCKER_NAME -aq) ]]; then
+  # Open a shell on that instance
+  docker start $DOCKER_NAME >/dev/null 2>&1
   docker exec -it $DOCKER_NAME bash
   exit 0
 fi
 
+# Create a brand new instance
 docker run -it --name $DOCKER_NAME -u $(id -u $USER):$(id -g $USER) -v ~/build:/build ubuntu
