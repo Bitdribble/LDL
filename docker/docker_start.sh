d35a76796c6983793b310441a31afa73bf18e8e6
Andrei Radulescu-Banu <44415560+andrei-radulescu-banu@users.noreply.github.com>
2021-12-31 20:02:01 -0500

    Devel

--- docker/docker_start.sh
+++ docker/docker_start.sh
@@ -12,10 +12,9 @@ if [[ $(docker ps --filter name=$DOCKER_NAME -aq) ]]; then
 fi
 
 # Create a brand new instance, but don't make it interactive yet
-docker run -it --name $DOCKER_NAME -v ~/build:/build -w /build $DOCKER_IMAGE
-# Install emacs
-docker exec $DOCKER_NAME "apt update; apt install -y emacs"
-
-# Enter a shell
-docker exec -it $DOCKER_NAME bash
-exit 0
+docker run -it \
+       --name $DOCKER_NAME \
+       -v ~/build:/build \
+       -w /build \
+       -e PYTHONPATH=/build/lse \
+       $DOCKER_IMAGE
