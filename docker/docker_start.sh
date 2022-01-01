b44539084291595b29c6d2128cfc62d6db6ffc88
Andrei Radulescu-Banu <andreiradulescu-banu@isee.ai>
2022-01-01 13:14:08 -0500

    Devel

--- docker/docker_start.sh
+++ docker/docker_start.sh
@@ -2,7 +2,12 @@
 
 DOCKER_CONTAINER_NAME=ldl
 DOCKER_IMAGE=ldl
+DOCKER_HOSTNAME=ldl
 WORKING_DIR=/build/LDL
+# $USER is set up by the shell
+USER_ID=$(id -u)
+GRP=$(id -g -n)
+GRP_ID=$(id -g)
 
 # Ensure a default display
 if [[ -z ${DISPLAY} ]];then
@@ -13,7 +18,7 @@ fi
 if [[ $(docker ps --filter name=$DOCKER_CONTAINER_NAME -aq) ]]; then
   # Open a shell on that instance
   docker start $DOCKER_CONTAINER_NAME >/dev/null 2>&1
-  docker exec -it $DOCKER_CONTAINER_NAME /bin/bash
+  docker exec -u $USER -it $DOCKER_CONTAINER_NAME /bin/bash
   exit 0
 fi
 
@@ -24,9 +29,17 @@ docker run \
        --name $DOCKER_CONTAINER_NAME \
        -v ~/build:/build \
        -w $WORKING_DIR \
-       -e DISPLAY=${DISPLAY} \
+       -e DISPLAY=$DISPLAY \
        -e PYTHONPATH=$WORKING_DIR \
+       --hostname $DOCKER_HOSTNAME \
        $DOCKER_IMAGE
+
+# Set up user and group
+if [[ "${USER}" != "root" ]]; then
+  docker exec $DOCKER_CONTAINER_NAME addgroup --force-badname --gid "${GRP_ID}" "${GRP}" >/dev/null
+  docker exec $DOCKER_CONTAINER_NAME adduser --force-badname --disabled-password --gecos '' $USER --uid $USER_ID --gid $GRP_ID >/dev/null
+  docker exec $DOCKER_CONTAINER_NAME usermod -aG sudo $USER >/dev/null
+fi
 
 # Allow X connections
 xhost +local:root 1>/dev/null 2>&1
@@ -36,6 +49,7 @@ docker exec \
        -e DISPLAY=$DISPLAY \
        -it \
        --privileged \
+       -u $USER \
        $DOCKER_CONTAINER_NAME \
        /bin/bash
 
