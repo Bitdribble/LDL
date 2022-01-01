d6b048c70318773f11e28d4fe39aefff4e2cc58f
Andrei Radulescu-Banu <andreiradulescu-banu@isee.ai>
2022-01-01 12:59:09 -0500

    Devel

--- docker/docker_start.sh
+++ docker/docker_start.sh
@@ -1,22 +1,43 @@
 #!/bin/bash
 
-DOCKER_NAME=ldl
+DOCKER_CONTAINER_NAME=ldl
 DOCKER_IMAGE=ldl
 WORKING_DIR=/build/LDL
 
+# Ensure a default display
+if [[ -z ${DISPLAY} ]];then
+  DISPLAY=":0"
+fi
+
 # Is an instance already running?
-if [[ $(docker ps --filter name=$DOCKER_NAME -aq) ]]; then
+if [[ $(docker ps --filter name=$DOCKER_CONTAINER_NAME -aq) ]]; then
   # Open a shell on that instance
-  docker start $DOCKER_NAME >/dev/null 2>&1
-  docker exec -it $DOCKER_NAME bash
+  docker start $DOCKER_CONTAINER_NAME >/dev/null 2>&1
+  docker exec -it $DOCKER_CONTAINER_NAME /bin/bash
   exit 0
 fi
 
-# Create a brand new instance, but don't make it interactive yet
-docker run -it \
-       --name $DOCKER_NAME \
+# Create a new instance, but keep it detached (-d)
+docker run \
+       -it \
+       -d \
+       --name $DOCKER_CONTAINER_NAME \
        -v ~/build:/build \
        -w $WORKING_DIR \
-       -e DISPLAY=:0 \
+       -e DISPLAY=${DISPLAY} \
        -e PYTHONPATH=$WORKING_DIR \
        $DOCKER_IMAGE
+
+# Allow X connections
+xhost +local:root 1>/dev/null 2>&1
+
+# Attach to the instance
+docker exec \
+       -e DISPLAY=$DISPLAY \
+       -it \
+       --privileged \
+       $DOCKER_CONTAINER_NAME \
+       /bin/bash
+
+# Disallow X connections
+xhost -local:root 1>/dev/null 2>&1
--- docker/docker_stop.sh
+++ docker/docker_stop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-DOCKER_NAME=ldl
+DOCKER_CONTAINER_NAME=ldl
 
-docker ps --filter name=$DOCKER_NAME -aq | xargs docker rm --force
+docker ps --filter name=$DOCKER_CONTAINER_NAME -aq | xargs docker rm --force 2>/dev/null
